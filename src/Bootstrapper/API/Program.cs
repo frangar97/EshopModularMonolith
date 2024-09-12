ffa55afa3e45ea2b5a9464812c945a2ad67ae18e
Francisco Garcia <97fran98@gmail.com>
2024-09-12 10:31:16 -0600

    [feat]: configure and create extension for carter

--- src/Bootstrapper/API/Program.cs
+++ src/Bootstrapper/API/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
+
+builder.Services.AddCarterWithAssemblies(typeof(CatalogModule).Assembly);
+
 builder.Services
     .AddBasketModule(builder.Configuration)
     .AddCatalogModule(builder.Configuration)
@@ -9,6 +12,10 @@ builder.Services
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app
+    .MapCarter();
+
 app
     .UseBasketModule()
     .UseCatalogModule()
---
+++ src/Shared/Shared/Extensions/CarterExtension.cs
@@ -0,0 +1,27 @@
+﻿using Carter;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Shared.Extensions
+{
+    public static class CarterExtension
+    {
+        public static IServiceCollection AddCarterWithAssemblies(this IServiceCollection services, params Assembly[] assemblies)
+        {
+            services.AddCarter(configurator: config =>
+            {
+
+                foreach (Assembly assembly in assemblies)
+                {
+                    Type[] modules = assembly.GetTypes()
+                    .Where(x => x.IsAssignableTo(typeof(ICarterModule))).ToArray();
+
+                    config.WithModules(modules);
+                }
+
+            });
+
+            return services;
+        }
+    }
+}
