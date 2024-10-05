8e6454884413e9b329c9ac61175704501781c837
Francisco Garcia <97fran98@gmail.com>
2024-10-05 14:53:59 -0600

    [feat]: generic model for pagination

--- src/Modules/Catalog/Catalog/Products/Features/GetProducts/GetProductsEndpoint.cs
+++ src/Modules/Catalog/Catalog/Products/Features/GetProducts/GetProductsEndpoint.cs
@@ -1,14 +1,16 @@
-﻿namespace Catalog.Products.Features.GetProducts
+﻿using Shared.Pagination;
+
+namespace Catalog.Products.Features.GetProducts
 {
-    public record GetProductsResponse(IEnumerable<ProductDto> Products);
+    public record GetProductsResponse(PaginatedResult<ProductDto> Products);
 
     public class GetProductsEndpoint : ICarterModule
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapGet("/products", async (ISender sender) =>
+            app.MapGet("/products", async ([AsParameters] PaginationRequest request, ISender sender) =>
             {
-                GetProductsResult result = await sender.Send(new GetProductsQuery());
+                GetProductsResult result = await sender.Send(new GetProductsQuery(request));
 
                 GetProductsResponse response = result.Adapt<GetProductsResponse>();
 
--- src/Modules/Catalog/Catalog/Products/Features/GetProducts/GetProductsHandler.cs
+++ src/Modules/Catalog/Catalog/Products/Features/GetProducts/GetProductsHandler.cs
@@ -1,19 +1,35 @@
-﻿namespace Catalog.Products.Features.GetProducts
+﻿using Shared.Pagination;
+
+namespace Catalog.Products.Features.GetProducts
 {
-    public record GetProductsQuery() : IQuery<GetProductsResult>;
-    public record GetProductsResult(IEnumerable<ProductDto> Products);
+    public record GetProductsQuery(PaginationRequest PaginationRequest)
+    : IQuery<GetProductsResult>;
+    public record GetProductsResult(PaginatedResult<ProductDto> Products);
     internal class GetProductsHandler(CatalogDbContext dbContext) : IQueryHandler<GetProductsQuery, GetProductsResult>
     {
         public async Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
         {
+            int pageIndex = request.PaginationRequest.PageIndex;
+            int pageSize = request.PaginationRequest.PageSize;
+
+            long totalCount = await dbContext.Products.LongCountAsync(cancellationToken);
+
             List<Product> products = await dbContext.Products
-                .AsNoTracking()
-                .OrderBy(x => x.Name)
-                .ToListAsync(cancellationToken);
+                            .AsNoTracking()
+                            .OrderBy(p => p.Name)
+                            .Skip(pageSize * pageIndex)
+                            .Take(pageSize)
+                            .ToListAsync(cancellationToken);
 
-            List<ProductDto> productsDto = products.Adapt<List<ProductDto>>();
+            List<ProductDto> productDtos = products.Adapt<List<ProductDto>>();
 
-            return new GetProductsResult(productsDto);
+            return new GetProductsResult(
+                new PaginatedResult<ProductDto>(
+                    pageIndex,
+                    pageSize,
+                    totalCount,
+                    productDtos)
+                );
         }
     }
 }
