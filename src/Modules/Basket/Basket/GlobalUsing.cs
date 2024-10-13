004d17a79da22426a5f0555cda5b2c04b534d313
Francisco Garcia <97fran98@gmail.com>
2024-10-12 19:54:33 -0600

    [feat]: creation of dtos and handlers for basket

--- src/Modules/Basket/Basket/GlobalUsing.cs
+++ src/Modules/Basket/Basket/GlobalUsing.cs
@@ -2,3 +2,9 @@
 global using Basket.Basket.Models;
 global using Microsoft.EntityFrameworkCore;
 global using System.Reflection;
+global using Basket.Basket.Dtos;
+global using Basket.Basket.Exceptions;
+global using Basket.Data;
+global using FluentValidation;
+global using Shared.CQRS;
+global using Mapster;
